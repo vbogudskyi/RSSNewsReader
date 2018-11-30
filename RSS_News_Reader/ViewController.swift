3564674b9217a00a7e6b6ebe16cc63f1f82acb69
Nicolae <nastas_colea@yahoo.com>
2018-11-30 14:53:07 -0500

    first commit

---
+++ RSS_News_Reader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RSS _News_Reader
+//
+//  Created by Nicolae Nastas on 2018-11-23.
+//  Copyright © 2018 Nicolae Nastas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    @IBOutlet var loginView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "General-background.png")!)
+        
+               loginView.backgroundColor = UIColor(patternImage: UIImage(named: "Login1-background.png")!)
+    }
+
+   
+    
+}
+
